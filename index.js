024718a5f98c10d75d633bbe25e7d1c07b60a49c
alexandra <alexandraschurman@gmail.com>
2022-01-10 15:20:01 -0800

    commit tree d30abae5959eaa97a4499743ed618457eef90824

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x) {
+  let distanceInFeet = x;
+  if (distanceInFeet <= 400) {
+    return "This one is on me!"
+  } else if (distanceInFeet >= 2001 && distanceInFeet < 2501) {
+    return "I will gladly take your thirty bucks."
+  } else (distanceInFeet >= 2501); {
+    return "No can do."
+  }
+};
+
+function ternaryCheckCity(cityName) {
+  return cityName == 'NYC' ? ("Ok, sounds good." ) : ("No go.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case'generous':
+      return 'Thank you so much.'
+    break;
+    case 'not as generous':
+      return 'Thank you.'
+    break;
+    default: 
+      return 'Bye.'  
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
+
+
+
+
+// function scuberGreetingForFeet(x){
+//   let distanceInFeet = x;
+//     if (distanceInFeet <= 400){
+//       return 'This one is on me!'
+//     } 
+//   else if (distanceInFeet >= 2001 && distanceInFeet <2501){
+//     return 'I will gladly take your thirty bucks.'
+//   }
+//   else (distanceInFeet >= 2501)
+//     return 'No can do.'
+//   }
+
+
+
+// function ternaryCheckCity(cityName){
+//     return cityName == 'NYC' ? ( "Ok, sounds good.") : ("No go.");
+//     // console.log(ternaryCheckCity(Pittsburgh))
+// }
+
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip){
+//     case "generous":
+//        return 'Thank you so much.'
+//       break;
+//     case "not as generous":
+//       return 'Thank you.'
+//       break;
+//     default:
+//       return 'Bye.'}
+  
+// }
